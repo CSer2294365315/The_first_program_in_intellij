859f7aaaad5ada7a40b52027a4b283ae67448829
CSer2294365315 <2294365315@qq.com>
2017-09-05 15:21:34 +0800

    menubar

--- src/J_9_3_01/J_9_5_6_MenuBar.java
+++ src/J_9_3_01/J_9_5_6_MenuBar.java
@@ -2,7 +2,7 @@ package J_9_3_01;
 
 public class J_9_5_6_MenuBar
 {
-    
+
 
 }
 
@@ -23,5 +23,11 @@ public void setJMenuBar(JMenuBar menubar)
 menubar - 放置于该窗体中的菜单栏
 
 
+JMenuItem
+
+public JMenuItem(Action a)
+创建从指定的 Action 获取其属性的菜单项。
+参数：
+a - JMenuItem 的操作
 
  */
